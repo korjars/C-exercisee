d6d7791727e81565a00cc30e8c840eca97f2eb98
zyichang <yichangzhang@outlook.com>
2021-08-02 14:08:56 +0800

    Daily Learning

---
+++ Algorithm/SequeueString.h
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<string.h>
+#include<strings.h>
+#include<stdbool.h>
+
+#define Maxlen 255
+
+typedef struct 
+{
+    char ch[Maxlen];
+    int length;
+}SString;
+
+typedef struct 
+{
+    char *ch;
+    int length;
+}HString;
+
+
+int next[];
+int nextval[];
+
--- Learn-C/H-Test.c
+++ Learn-C/H-Test.c
@@ -1,5 +1,6 @@
 #include "Test.h"
 #include<stdio.h>
+//void PrintTestFile();
 int main()
 {
     PrintTestFile();
