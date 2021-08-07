f0d699b3a6b525dcdf8c728dd727641bdfc08379
zyichang <yichangzhang@outlook.com>
2021-08-07 21:04:43 +0800

    learnning algorithm

--- Algorithm/BiTree.c
+++ Algorithm/BiTree.c
@@ -3,6 +3,7 @@
 #include<string.h>
 #include<math.h>
 
+
 typedef struct Treebasic
 {
 	int date;
@@ -10,3 +11,17 @@ typedef struct Treebasic
 	struct Treebasic *rchlid;
 
 }BiTnode, *BiTree;
+
+void visit(BiTree T)
+{
+	printf("-%d-",T->date);
+}
+
+void ProOrder(BiTree T)
+{
+	if (T)
+	{
+		/* code */
+	}
+	
+}
